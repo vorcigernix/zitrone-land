907c15f614dfda1870e942c2be91faa1535d2f9b
Adam Sobotka <vorcigernix@gmail.com>
2022-06-29 18:38:18 +0200

    icons

--- routes/index.tsx
+++ routes/index.tsx
@@ -83,7 +83,7 @@ export default function Home() {
 
 							<a
 								href='#features'
-								class={tw`block w-full px-12 py-3 text-sm font-medium text-white border border-green-600 rounded sm:w-auto hover:bg-green-600 active:bg-blue-500 focus:outline-none focus:ring`}>
+								class={tw`block w-full px-12 py-3 text-sm font-medium text-white border border-green-300 rounded sm:w-auto hover:bg-green-600 active:bg-blue-500 focus:outline-none focus:ring`}>
 								Features
 							</a>
 						</div>
@@ -111,13 +111,10 @@ export default function Home() {
 								fill='none'
 								viewBox='0 0 24 24'
 								stroke='currentColor'>
-								<path d='M12 14l9-5-9-5-9 5 9 5z' />
-								<path d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z' />
 								<path
 									stroke-linecap='round'
 									stroke-linejoin='round'
-									stroke-width='2'
-									d='M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222'
+									d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
 								/>
 							</svg>
 
@@ -138,13 +135,10 @@ export default function Home() {
 								fill='none'
 								viewBox='0 0 24 24'
 								stroke='currentColor'>
-								<path d='M12 14l9-5-9-5-9 5 9 5z' />
-								<path d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z' />
 								<path
 									stroke-linecap='round'
 									stroke-linejoin='round'
-									stroke-width='2'
-									d='M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222'
+									d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
 								/>
 							</svg>
 
@@ -165,13 +159,10 @@ export default function Home() {
 								fill='none'
 								viewBox='0 0 24 24'
 								stroke='currentColor'>
-								<path d='M12 14l9-5-9-5-9 5 9 5z' />
-								<path d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z' />
 								<path
 									stroke-linecap='round'
 									stroke-linejoin='round'
-									stroke-width='2'
-									d='M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222'
+									d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
 								/>
 							</svg>
 
@@ -226,7 +217,7 @@ export default function Home() {
 
 						<a
 							href='https://zitrone.vercel.app/'
-							class={tw`block w-full px-12 py-3 text-sm font-medium text-white border border-green-600 rounded sm:w-auto hover:bg-green-600 active:bg-blue-500 focus:outline-none focus:ring`}>
+							class={tw`block w-full px-12 py-3 text-sm font-medium text-white border border-green-300 rounded sm:w-auto hover:bg-green-600 active:bg-blue-500 focus:outline-none focus:ring`}>
 							Try Beta
 						</a>
 					</div>
